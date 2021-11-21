5bc4087832a434f7800082f045b01418244b9933
drewosmond <drewosmond@gmail.com>
2021-11-21 15:30:23 -0800

    updated home page

--- web/src/components/home/home.js
+++ web/src/components/home/home.js
@@ -5,19 +5,12 @@ const Home = () => {
   const navigate = useNavigate();
   const canvases = useSelector((state) => state.canvases);
 
-  const fakeUser = {
-    username: "ligma",
-  };
-
   return (
     <>
       <button
         onClick={() => navigate(canvases.length > 1 ? "/library" : "/draw")}
       >
         go draw :D
-      </button>
-      <button onClick={() => navigate(`/profile/${fakeUser.username}`)}>
-        profile
       </button>
     </>
   );
