0edbad8bf23f40f24f0042c8fa9cd2e30ed32291
drewosmond <drewosmond@gmail.com>
2021-11-23 16:56:34 -0800

    added more styling to landing page

--- web/src/components/home/home.js
+++ web/src/components/home/home.js
@@ -2,11 +2,14 @@ import { useNavigate } from "react-router";
 import { useSelector } from "react-redux";
 import "./home.css";
 
-import familiarToolsSVG from "../../assets/familiarTools.svg";
-import colorAndOpacitySVG from "../../assets/color&opacity.svg";
-import librarySVG from "../../assets/library.svg";
-import layersSVG from "../../assets/layersHome.svg";
-import downloadSVG from "../../assets/download.svg";
+// import familiarToolsSVG from "../../assets/familiarTools.svg";
+// import colorAndOpacitySVG from "../../assets/color&opacity.svg";
+// import librarySVG from "../../assets/library.svg";
+// import layersSVG from "../../assets/layersHome.svg";
+// import downloadSVG from "../../assets/download.svg";
+
+import homePageSVG from "../../assets/feature-list.svg";
+import landingpageSVG from "../../assets/landingpage-text-version2.svg";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -14,11 +17,15 @@ const Home = () => {
 
   return (
     <div className="home-div">
-      <div>
-        <p className="title">bixels</p>
-        <p className="title-description">simply create.</p>
-        <p className="title-description-next-line"> pixel art</p>
+      {/* <div> */}
+      <div className="logo-container">
+        <img className="logo-container" src={landingpageSVG} alt="banner" />
       </div>
+      {/* <div classname="logo-container">
+        <div className="title">bixels</div>
+        <div className="title-description">simply create.</div>
+        <div className="title-description-next-line"> pixel art</div>
+      </div> */}
       <div className="title-button-div">
         {/* <button className="title-button" onClick={() => navigate("/library")}>
           let's get started!
@@ -32,7 +39,8 @@ const Home = () => {
       </div>
       <div className="smaller-screen-container">
         <div className="smaller-screen-home">
-          <img
+          <img src={homePageSVG} alt="page descripton" />
+          {/* <img
             className="familiar-tools-home"
             src={familiarToolsSVG}
             alt="familiar tools"
@@ -57,7 +65,7 @@ const Home = () => {
             className="download-home"
             src={downloadSVG}
             alt="download your images"
-          />
+          /> */}
         </div>
       </div>
     </div>
